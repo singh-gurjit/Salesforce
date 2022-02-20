7dec9700e700f70cbbff1452ed7161499855a304
Gurjit Singh1 <gurjit.singh1@ibm.com>
2022-02-20 13:47:29 +0530

    code added for aura

--- extension.js
+++ extension.js
@@ -1,8 +1,8 @@
 const fileConfiguration = vscode.workspace.getConfiguration('files');
 const associations = fileConfiguration.get('associations');
 
-associations['*.app'] = "auraComponent";
-associations['*.cmp'] = "auraComponent";
+associations['*.app'] = "auraComponent-markup";
+associations['*.cmp'] = "auraComponent-markup";
 
 fileConfiguration.update('associations', associations, vscode.ConfigurationTarget.Global)
     .then(() => {
