6665abdc094d04b9b3404084bc45a21db1f57db6
Gurjit Singh <gurjit.singh@gartner.com>
2022-08-24 23:41:13 +0530

    removed aura fileassociations

--- extension.js
+++ extension.js
@@ -3,8 +3,8 @@ const vscode = require("vscode");
 const fileConfiguration = vscode.workspace.getConfiguration('files');
 const associations = fileConfiguration.get('associations');
 
-associations['*.app'] = "auraComponent";
-associations['*.cmp'] = "auraComponent";
+/* associations['*.app'] = "auraComponent";
+associations['*.cmp'] = "auraComponent"; */
 
 fileConfiguration.update('associations', associations, vscode.ConfigurationTarget.Global)
     .then(() => {
